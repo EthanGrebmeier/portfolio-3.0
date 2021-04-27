a5dd874a535fdb628ae21d94caf305af20a58ee7
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-04-27 11:00:24 -0700

    Moved experience to its own file

---
+++ src/experience.js
@@ -0,0 +1,34 @@
+const experience = [
+    {
+        color:'var(--primary)',
+        title:'The Future is Good', 
+        position:'Web Developer',
+        length:'Jun ‘20 - Present',
+        content:'The Future is Good (The FIG) is an online marketplace, tool, and resource that helps customers to actively reduce their carbon footprint.',
+        subtext:'React, Node, Express, PostgreSQL',
+        href: '/experience/thefig',
+        id: 'thefig'
+    },
+    {
+        color:'var(--primary)',
+        title:'Mustang Media Group', 
+        position:'Web Developer',
+        length:'Nov ‘20 - Mar ‘21',
+        content:'Mustang Media Group oversees Cal Poly’s print and online news, as well as KCPR, the school‘s radio station.',
+        subtext:'React, Node, Express, MongoDB, Google Cloud, Wordpress',
+        href: '/experience/mustangmedia',
+        id: 'mustangmedia'
+    },
+    {
+        color:'var(--primary)',
+        title:'ShipOffers', 
+        position:'Integrated Logistics Support Specialist',
+        length:'Nov ‘19 - Present',
+        content:'Shipoffers is one of the top shipping and fulfillment companies, working primariliy with health and beauty products.',
+        subtext:'Python, PostgreSQL',
+        href: '/experience/shipoffers',
+        id: 'shipoffers'
+    },
+] 
+
+export default experience
