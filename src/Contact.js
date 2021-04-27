ea7c23e19ffe9aa419225a49521a9a6134a49666
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-04-27 10:54:43 -0700

    Removed gap and added functionality

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import styled from 'styled-components'
-
+import axios from 'axios'
 
 const Wrapper = styled.form`
+    margin-top: 20px;
     width: 100%;
     display: flex;
     flex-direction: column;
-    gap: 10px;
-    background: var(--primaryTwo);
+    background: var(--secondary);
     border-radius: 12px;
     box-sizing: border-box;
     padding: 20px;
@@ -16,13 +16,13 @@ const Row = styled.div`
     display: flex;
     width: 100%;
     justify-content: space-between;
+    margin-bottom: 10px;
     
 `
 
 const Label = styled.label`
     display: flex;
     flex-direction: column;
-    gap: 8px;
     color: white;
     font-size: 24px;
     width: 100%;
@@ -36,6 +36,7 @@ const Input = styled.input`
     border: none;
     padding: 10px;
     box-sizing: border-box;
+    margin-top: 8px;
 `
 
 const Body = styled.textarea`
@@ -43,7 +44,35 @@ const Body = styled.textarea`
     border: none;
     padding: 10px;
     box-sizing: border-box;
-    font-family: "Montserrat"
+    font-family: "Montserrat";
+    resize: none;
+    margin-top: 8px;
+`
+
+const Button = styled.button`
+    width: 150px;
+    height: 40px;
+    background: transparent;
+    border: 2px solid white;
+    border-radius: 12px;
+    color: white;
+    font-family: Montserrat;
+    font-size: 24px;
+    font-weight: 400;
+    cursor: pointer;
+    :hover{
+        border: 4px solid white;
+    }
+    transition: border .1s linear;
+`
+
+const Status = styled.p`
+    display:flex;
+    align-items: center;
+    height: 40px;
+    width: 300px; 
+    color: white;
+    font-size: 24px;
 `
 
 const Contact = () => {
@@ -51,8 +80,25 @@ const Contact = () => {
     const [inputEmail, setInputEmail] = useState('')
     const [inputBody, setInputBody] = useState('')
 
+    const [status, setStatus] = useState('')
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+        if (!inputBody || !inputEmail || !inputName){
+            return setStatus('Please fill in each field')
+        }
+        setStatus('Sending...')
+        axios.post('https://eg-portfolio-server.herokuapp.com/sendMail', {
+            name: inputName,
+            email: inputEmail,
+            body: inputBody
+        }).then(res => {
+            setStatus('Email Sent!')
+        }).catch(err => setStatus('Something went wrong'))
+    }
+
     return (
-        <Wrapper>
+        <Wrapper onSubmit={onFormSubmit}>
             <Row>
                 <Label>
                     Name
@@ -67,6 +113,12 @@ const Contact = () => {
                 Body
                 <Body onChange={e => setInputBody(e.target.value)} rows='10'/>
             </Label>
+            <Status>
+                {status}
+            </Status>
+            <Button>
+                Send
+            </Button>
         </Wrapper>
     )
 }
