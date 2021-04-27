bf0e41a7bc5d8898d67ded85c86ad5d91fad9e82
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-04-27 11:10:27 -0700

    Moved projects to their own file

---
+++ src/projects.js
@@ -0,0 +1,40 @@
+import Traace from './articles/Traace'
+
+const projects = [
+    {
+        title: 'Traace.io',
+        content: 'A web-application for Covid-19 Contact Tracing',
+        subtext: 'React, Node, Express, PostgreSQL, Google Cloud',
+        href: '/project/traace',
+        id: 'traace',
+        demo: 'https://www.traace.io',
+        github: 'https://github.com/EthanGrebmeier/contact-tracing-server',
+        article: Traace
+    },
+    {
+        title: 'Sorter',
+        content: 'A sorting algorithm visualizer',
+        subtext: 'React',
+        href: '/project/sorter',
+        id: 'sorter',
+        github: 'https://github.com/EthanGrebmeier/react-sorter'
+    },
+    {
+        title: 'Say Hello',
+        content: 'A lightweight Twitter clone',
+        subtext: 'React, Node, Express, MongoDB',
+        href: '/project/say-hello',
+        id: 'say-hello',
+        github: 'https://github.com/EthanGrebmeier/react-say-hello'
+    },
+    {
+        title: 'Reader',
+        content: 'A speed reading assistant',
+        subtext: 'React',
+        href: '/project/reader',
+        id: 'reader',
+        github: 'https://github.com/EthanGrebmeier/react-reader'
+    },
+]
+
+export default projects
